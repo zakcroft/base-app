dac92b79accb07c5b5657329891952c7749317b6
zak <zakcroft@gmail.com>
2017-10-01 14:28:16 +0100

    setup

---
+++ src/components/App/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { render } from 'react-dom';
+//import { Provider, connect } from 'react-redux';
+//import { ThemeProvider } from 'styled-components';
+
+if (module && module.hot) {
+  module.hot.accept();
+}
+
+//console.log(`Looks like we are in ${process.env.NODE_ENV} mode!`);
+
+class App extends Component {
+  // static propTypes = {};
+  //
+  // static defaultProps = {
+  //   hideSideMenu: false,
+  //   auth: {
+  //     loading: true
+  //   }
+  // };
+
+  render() {
+    return (
+      <main>
+        <ul>
+          <li>Test</li>
+        </ul>
+      </main>
+    );
+  }
+}
